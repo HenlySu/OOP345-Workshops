5472e2e3aa410ad4b0b8dedcf1b311868d7f59d9
HenlySuu <hsu31@myseneca.ca>
2022-11-06 01:51:22 -0400

    Update

---
+++ Workshop 6/p1/Utilities.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+
+#include "Vehicle.h"
+
+namespace sdds {
+   Vehicle* createInstance(std::istream& in);
+}
+#endif // !SDDS_UTILITIES_H
+
+
+
+
