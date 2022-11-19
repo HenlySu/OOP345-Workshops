367ecc38a6be6eed59266c815719cd16160435ec
HenlySuu <hsu31@myseneca.ca>
2022-11-19 02:05:25 -0500

    update

--- Workshop 8/p1/WritingRecord.cpp
+++ Workshop 8/p1/WritingRecord.cpp
@@ -8,20 +8,18 @@ namespace sdds {
 	GeneratingList<EmployeeWage> writeRaw(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal) {
 		GeneratingList<EmployeeWage> activeEmp;
 
-		// TODO: Add your code here to build a list of employees
-		//         using raw pointers
+		for (size_t i = 0; i < emp.size(); i++) {
+			for (size_t j = 0; j < sal.size(); j++) {
 
-		for (size_t i = 0; i < emp.size(); i++)
-		{
-			for (size_t j = 0; j < sal.size(); j++)
-			{
 				if (emp[i].id == sal[j].id) {
-					EmployeeWage ew(emp[i].name, sal[j].salary);
-					ew.rangeValidator();
+
 					if (!activeEmp.validation_Luhn_ALgorithm(emp[i].id))
-						throw std::string("*** Wrong Salaries with SIN No's");
-					EmployeeWage* p = &ew;
-					activeEmp += p;
+						throw std::string("Error!");
+
+					EmployeeWage empWage(emp[i].name, sal[j].salary);
+					empWage.rangeValidator();								
+
+					activeEmp += &empWage;
 				}
 			}
 		}
