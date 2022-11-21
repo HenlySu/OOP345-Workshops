882e79c1ec0ac317f49b4e6de965019a009f4174
HenlySuu <hsu31@myseneca.ca>
2022-11-20 23:46:32 -0500

    Testing functions

--- Sandbox/sandbox.cpp
+++ Sandbox/sandbox.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
-#include <vector>
 
-using namespace std;
-
-int main() {
-   vector<int> v1 = { 1,2,3 };
-   v1.push_back(9);
+int main()
+{
    
-   for (int i = 0; i < v1.size(); i++) {
-      cout << v1[i] << endl;
-   }
-
-   v1.pop_back();
-   cout << "Popping from vector" << endl;
-   for (int i = 0; i < v1.size(); i++) {
-      cout << v1[i] << endl;
-   }
-
-   v1.capacity(); //Returns the capacity of the vector. It doubles in size when it reaches its limits
-                  //
+   return 0;
 }
