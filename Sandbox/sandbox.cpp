597ced866115b58e8a4460c36da81a66a634a6b7
HenlySuu <hsu31@myseneca.ca>
2022-12-01 02:34:24 -0500

    update

--- Sandbox/sandbox.cpp
+++ Sandbox/sandbox.cpp
@@ -1,59 +1,16 @@
+#include <vector>
+#include <algorithm>
 #include <iostream>
+#include<array>
+
 using namespace std;
 
-class B1 {
-   int data;
-public:
-   B1(int v = 0) :data(v) {
-      cout << "[CB1: " << data << "]\n";
-   }
-   virtual ~B1() {
-      cout << "[~B1: " << data << "]\n";
-   }
-   virtual ostream& show(ostream& os)const {
-      return os << "[B1: " << data << "]";
-   }
-};
+int main()
+{
+   std::array<int, 10> a = { 1,1,1,1,2,2,2,3,3,4 };
+   int m = count_if(a.begin(), a.end(), [](int val) {return val < 2; });
 
-ostream& operator<<(ostream& os, const B1& B) {
-   return B.show(os);
-}
+   cout << m;
 
-class B2 {
-   int data;
-public:
-   B2(int v = 0) :data(v) {
-      cout << "[CB2: " << data << "]\n";
-   }
-   virtual ~B2() {
-      cout << "[~B2: " << data << "]\n";
-   }
-   virtual ostream& show(ostream& os)const {
-      return os << "[B2: " << data << "]";
-   }
-};
-
-ostream& operator<<(ostream& os, const B2& B) {
-   return B.show(os);
-}
-
-class Derived :public B2, public B1 {
-   int data;
-public:
-   Derived(int v = 100) :B1(v + 1), B2(v + 2), data(v) {
-      cout << "[CD: " << data << "]\n";
-   }
-   virtual ~Derived() {
-      cout << "[~D: " << data << "]\n";
-   }
-   virtual ostream& show(ostream& os)const {
-      B1::show(os);
-      B2::show(os);
-      return os << "[D: " << data << "]";
-   }
-};
-
-int main() {
-   Derived D(10);
    return 0;
 }
