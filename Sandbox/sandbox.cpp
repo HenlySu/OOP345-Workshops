cd0e72be6a1e1bde61991fd336ab1210ce4f4550
HenlySuu <hsu31@myseneca.ca>
2022-12-01 02:43:14 -0500

    Update

--- Sandbox/sandbox.cpp
+++ Sandbox/sandbox.cpp
@@ -1,16 +1,15 @@
-#include <vector>
-#include <algorithm>
 #include <iostream>
-#include<array>
+#include <algorithm>
+#include <functional>
+
+#include <vector>
+#include <array>
+#include <list>
 
 using namespace std;
 
 int main()
 {
-   std::array<int, 10> a = { 1,1,1,1,2,2,2,3,3,4 };
-   int m = count_if(a.begin(), a.end(), [](int val) {return val < 2; });
-
-   cout << m;
-
+   
    return 0;
 }
