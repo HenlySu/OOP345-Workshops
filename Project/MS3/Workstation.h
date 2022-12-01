7d1a0e0e6a0dfa58f0eaaf6b9dd0e2c471fa3678
HenlySuu <hsu31@myseneca.ca>
2022-12-01 16:06:00 -0500

    update

---
+++ Project/MS3/Workstation.h
@@ -0,0 +1,27 @@
+#ifndef SDDS_WORKSTATION_H
+#define SDDS_WORKSTATION_H
+
+#include <iostream>
+#include <deque>
+#include"CustomerOrder.h"
+
+namespace sdds {
+   //Global Variables
+   extern std::deque<sdds::CustomerOrder> g_pending;
+   extern std::deque<sdds::CustomerOrder> g_completed;
+   extern std::deque<sdds::CustomerOrder> g_incomplete;
+
+   class Workstation : public Station{
+      std::deque<CustomerOrder> m_orders{};
+      Workstation* m_pNextStation{};
+   public:
+      Workstation(std::string& str);
+      void fill(std::ostream& os);
+      bool attemptToMoveOrder();
+      void setNextStation(Workstation* station);
+      Workstation* getNextStation() const;
+      void display(std::ostream& os) const;
+      Workstation& operator+=(CustomerOrder&& newOrder);
+   };
+}
+#endif // !SDDS_WORKSTATION_H
